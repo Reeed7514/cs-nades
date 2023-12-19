e7b5ef108d47eac1190f9b08beaa823fa4022251
jonabird <bearinparadox@gmail.com>
2023-12-19 22:24:15 +0800

    add mapicons and map selector

---
+++ src/components/map/MapSelector.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-col border border-slate-300 rounded-md w-[150px]">
+    <div class="px-4 py-2 text-center bg-orange-300">
+      地图
+    </div>
+
+    <div v-for="map in maps" class="flex gap-3 items-center px-4 py-2 border-t border-t-slate-200 hover:bg-gray-200 hover:cursor-pointer">
+      <img :src="'/mapicons/' + map + '.webp'" class="w-6 h-auto">
+      <p>{{ map }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue"
+const maps = [
+  "mirage",
+  "inferno",
+  "overpass",
+  "ancient",
+  "nuke",
+  "vertigo",
+  "anubis",
+]
+</script>
+
+<style scoped></style>
--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="mt-16">
-    
+  <div class="mt-16 flex justify-center pt-4">
+    <MapSelector />
   </div>
 </template>
 
 <script setup lang="ts">
-
+import MapSelector from "../components/map/MapSelector.vue"
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
